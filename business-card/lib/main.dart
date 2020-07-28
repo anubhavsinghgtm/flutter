1e7f8aadedb1cf97351cdc90a4bd119850b68152
Anubhav Singh <anubhavsinghgtm@gmail.com>
2020-07-28 13:39:12 +0530

    Create main.dart

---
+++ business-card/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:profile_app/ui/home.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return myApp();
+  }
+}
